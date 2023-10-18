5f5e002a840d2f49d9980860e0d376c5708196f3
Ansh Aneja <128882734+anshaneja5@users.noreply.github.com>
2023-10-19 00:19:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const hours= document.querySelector("[data-hours]");
+const minutes= document.querySelector("[data-minutes]");
+const seconds= document.querySelector("[data-seconds]");
+
+setInterval(setClock,1000);
+
+function setClock(){
+    const currentDate=new Date();
+    const secondsRatio=currentDate.getSeconds()/60;
+    const minutesRatio=(secondsRatio+currentDate.getMinutes())/60;
+    const hoursRatio=(minutesRatio +currentDate.getHours())/60;
+
+    setRotation(hours,hoursRatio);
+    setRotation(minutes,minutesRatio);
+    setRotation(seconds,secondsRatio);
+}
+
+
+function setRotation(element,ratio){
+    element.style.setProperty("--rotation",ratio*360);
+}
+
+setClock()
